c5e27fa2a01eb3c7961ddb148970cf42fd63538a
Gara Luzardo <garaluzardo@gmail.com>
2025-02-14 15:14:01 +0000

    Iteration 2

--- src/components/TravelList.jsx
+++ src/components/TravelList.jsx
@@ -6,13 +6,20 @@ function TravelList() {
 
     return (
         <div>
-            <h2>Tailored Travel Plans for Ironhackers</h2>
+        
             <ul>
                 {travelPlans.map((plan) => (
                     <li key={plan.id}>
                         <h3>{plan.destination}</h3>
+                        <img src={plan.image} alt={plan.destination} className="travel-image" />
                         <p>{plan.description}</p>
-                        <p><strong>Precio:</strong> {plan.price}€</p>
+                        <p><strong>Días:</strong> {plan.days}</p>
+                        <p><strong>Precio:</strong> {plan.totalCost}€</p>
+
+                        {/*Iteration 2 - labels based on the cost of each travel plan*/}
+                        {plan.totalCost <= 350 && <span className="label great-deal">Great Deal</span>}
+                        {plan.totalCost >= 1500 && <span className="label premium">Premium</span>}
+                        {plan.allInclusive && <span className="label all-inclusive">All Inclusive</span>}
                     </li>
                 ))}
             </ul>
